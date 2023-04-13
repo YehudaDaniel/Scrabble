40ea04a37cfbc9e974843665fd1f13799a3c84e9
Yehuda Daniel <h10ytguf@gmail.com>
2023-04-13 13:54:12 +0300

    pushing changes to the folders hierarchy, all files from previous semester went into 'backend' folder, and new files from this semester will go into 'frontend'

---
+++ src/backend/MyServer.java
@@ -0,0 +1,60 @@
+package backend;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class MyServer {
+    int port;
+    boolean stop;
+    ClientHandler ch;
+
+    public MyServer(int port, ClientHandler ch){
+        this.port=port;
+        this.ch=ch;
+    }
+    public void start(){
+        stop=false;
+        new Thread(()->startServer()).start();
+    }
+    private void startServer(){
+        ServerSocket server=null;
+        Socket client=null;
+        try {
+            server=new ServerSocket(port);
+            server.setSoTimeout(1000);
+            while(!stop){
+                try{
+                    client=server.accept();
+                    ch.handleClient(client.getInputStream(), client.getOutputStream());
+                    ch.close();
+                    client.close();}
+                catch (SocketTimeoutException e){}
+            }
+        } catch (IOException e) {
+            //throw new RuntimeException(e);
+            e.printStackTrace();
+        }
+        finally {
+            ch.close();
+            try {
+                server.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                client.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public void close(){
+        stop();
+    }
+	public void stop(){
+        stop=true;
+    }
+}
---
+++ src/frontend/main.java
@@ -0,0 +1,7 @@
+package frontend;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Front-end");
+    }
+}
