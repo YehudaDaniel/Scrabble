a9928d689d8f7b412f2ef2c956a77033342a84f2
Yehuda Daniel <h10ytguf@gmail.com>
2023-05-10 11:52:24 +0300

    adding comment

--- src/server/IOSearcher.java
+++ src/server/IOSearcher.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 
+//Given a word and a file, searches the whole file for the word given and returns true if found, false otherwise
 public class IOSearcher {
     public static boolean search(String word, String...fileNames){
         Scanner scanner=null;
