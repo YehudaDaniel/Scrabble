097b12b658ed389879d59cae12f37a609bb3bfcd
jagashot <ilanbrilovitch@gmail.com>
2023-06-30 17:22:49 +0300

    Ilan - changing runGame() in GameManager

--- PTM/src/Model/GameManager.java
+++ PTM/src/Model/GameManager.java
@@ -11,6 +11,24 @@ public class GameManager
     }
 
     public void runGame() {
+        public void runGame() {
+            // Check if the host is ready
+            if (host.isReady()) {
+                // Start the game
+                host.startGame();
+
+                // Run the game loop for the specified number of rounds
+                for (int i = 0; i < host.getRounds(); i++) {
+                    // Perform game logic for each round
+                    host.playRound();
+                }
+
+                // End the game
+                host.endGame();
+            } else {
+                System.out.println("The host is not ready. Please ensure all necessary configurations are set up.");
+            }
+        }
     }
 }
 
